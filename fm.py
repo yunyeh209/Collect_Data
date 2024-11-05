9111c26d6b80df9894c39c906b970395db8185b6
Kevin <135311198+Hsukevin121@users.noreply.github.com>
2024-11-05 14:05:16 +0800

    增加轉發到AI平台

--- fm.py
+++ fm.py
@@ -8,24 +8,16 @@ import pymysql
 
 # 获取当前时间的时间戳（精确到微秒）
 timestamp = datetime.utcnow().timestamp()
-# 将时间戳转换为秒
 time_in_s = int(timestamp)
-# 获取毫秒部分
 time_ms = int((timestamp - time_in_s) * 1000000)
-# 格式化时间
 event_time = datetime.utcfromtimestamp(time_in_s).strftime('%Y-%m-%dT%H:%M:%S') + f'.{time_ms:06d}Z'
 
-# 取整到最近的 900 秒的倍数
 collection_end_time = int(timestamp - (timestamp % 900))
-# 计算 collectionEndTime 前 900 秒的时间戳
 collection_start_time = collection_end_time - 900
-# 将 collectionStartTime 和 collectionEndTime 转换成微秒级别
 collection_start_time_micros = collection_start_time * 1000000
 collection_end_time_micros = collection_end_time * 1000000
-# 将 collectionStartTime 和 collectionEndTime 格式化成可读的时间字符串
 interval_start_time = datetime.utcfromtimestamp(collection_start_time).strftime('%a, %d %b %Y %H:%M:%S GMT')
 interval_end_time = datetime.utcfromtimestamp(collection_end_time).strftime('%a, %d %b %Y %H:%M:%S GMT')
-
 
 def generate_event_id(prefix='fault_', length=10):
     random_string = ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
@@ -59,7 +51,6 @@ fault_to_device_mapping = {
     197: ('DU', 3),
     198: ('DU', 3),
     199: ('CU', 3),
-    # 添加更多映射...
 }
 
 # Update MySQL Device Status Based on Fault ID
@@ -73,7 +64,6 @@ def update_device_status(fault_id, device_type, new_status, is_cleared):
 
     try:
         with db_connection.cursor() as cursor:
-            # 如果故障未清除 (is_cleared == "false")，将 message 设置为 fault_id
             if is_cleared == "false":
                 message = f"Fault detected, Fault ID: {fault_id}"
                 new_status = 3
@@ -82,29 +72,26 @@ def update_device_status(fault_id, device_type, new_status, is_cleared):
                 new_status = 1
 
             if device_type == 'RU_DU':
-                # 更新 RU 和 DU 设备的状态、fault_id 和 message
                 cursor.execute(
-                    "UPDATE devicelist SET status = %s, message = %s WHERE devicename LIKE 'CU%%'", 
+                    "UPDATE devicelist SET status = %s, message = %s WHERE devicename LIKE 'CU%%'",
                     (new_status, message)
                 )
                 cursor.execute(
-                    "UPDATE devicelist SET status = %s, message = %s WHERE devicename LIKE 'CU%%'", 
+                    "UPDATE devicelist SET status = %s, message = %s WHERE devicename LIKE 'CU%%'",
                     (new_status, message)
                 )
             elif device_type == 'DU_CU':
-                # 更新 DU 和 CU 设备的状态、fault_id 和 message
                 cursor.execute(
-                    "UPDATE devicelist SET status = %s, message = %s WHERE devicename LIKE 'CU%%'", 
+                    "UPDATE devicelist SET status = %s, message = %s WHERE devicename LIKE 'CU%%'",
                     (new_status, message)
                 )
                 cursor.execute(
-                    "UPDATE devicelist SET status = %s, message = %s WHERE devicename LIKE 'CU%%'", 
+                    "UPDATE devicelist SET status = %s, message = %s WHERE devicename LIKE 'CU%%'",
                     (new_status, message)
                 )
             else:
-                # 更新指定设备类型的状态、fault_id 和 message
                 cursor.execute(
-                    "UPDATE devicelist SET status = %s, message = %s WHERE devicename LIKE %s", 
+                    "UPDATE devicelist SET status = %s, message = %s WHERE devicename LIKE %s",
                     (new_status, message, f'{device_type}%')
                 )
 
@@ -161,58 +148,87 @@ def generate_json(mqtt_data):
     print("Generated JSON template:", template)
     return template
 
-# Send HTTP POST Request
-def send_http_request(payload):
-    try:
-        # 设置目标地址
-        url = 'http://192.168.0.39:30417/eventListener/v7'
+# POST设备数据到上层API
+def post_fault_data(device_type, DeviceId, alarm_data):
+    base_url = "http://192.168.0.25/api/v1/ORAN/O1Fault"
+    server_id = 10001
+    url = f"{base_url}?serverid={server_id}"
 
-        # 设置用户名和密码
-        username1 = 'sample1'
-        password1 = 'sample1'
+    headers = {'Content-Type': 'application/json'}
 
-        headers = {'content-type': 'application/json'}
-        verify = False
+    formatted_data = {
+        "DeviceId": DeviceId,
+        "DeviceType": device_type,
+        "AlarmId": alarm_data.get("AlarmId", 0),
+        "EventTime": alarm_data.get("EventTime", ""),
+        "EventSeverity": alarm_data.get("EventSeverity", ""),
+        "SystemDN": alarm_data.get("SystemDN", ""),
+        "AlarmType": "oam_Alarms",
+        "ProbableCause": alarm_data.get("ProbableCause", ""),
+        "IsCleared": alarm_data.get("IsCleared", "")
+    }
 
-        # 发送 HTTP POST 请求
-        response = requests.post(url, json=payload, auth=(username1, password1), headers=headers, verify=verify)
+    response = requests.post(url, headers=headers, json=formatted_data)
+    if response.status_code == 200:
+        print(f"Success: {response.json()}")
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
 
-        # 检查响应状态码
-        if response.status_code >= 200 and response.status_code <= 300:
-            print(payload)
-            print('Data sent successfully')
+# 根据 fault_id 处理不同设备类型的告警
+def handle_fault(fault_id, is_cleared, mqtt_data):
+    if fault_id in fault_to_device_mapping:
+        device_type, _ = fault_to_device_mapping[fault_id]
+
+        # 设置告警事件数据
+        alarm_data = {
+            "AlarmId": fault_id,
+            "EventTime": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "EventSeverity": mqtt_data['notification']['alarm-notif']["fault-severity"],
+            "SystemDN": "gregins",
+            "ProbableCause": mqtt_data['notification']['alarm-notif']["fault-text"],
+            "IsCleared": "Idle" if is_cleared == "false" else "Active"
+        }
+
+        # 根据设备类型设置 DeviceId 并发送不同的数据包
+        if device_type == "RU":
+            post_fault_data("RU01001", 2001, alarm_data)
+        elif device_type == "DU":
+            post_fault_data("DU01001", 3001, alarm_data)
+        elif device_type == "CU":
+            post_fault_data("CU01001", 4001, alarm_data)
+        elif device_type == "RU_DU":
+            post_fault_data("RU01001", 2001, alarm_data)  # RU设备数据包
+            post_fault_data("DU01001", 3001, alarm_data)  # DU设备数据包
+        elif device_type == "DU_CU":
+            post_fault_data("DU01001", 3001, alarm_data)  # DU设备数据包
+            post_fault_data("CU01001", 4001, alarm_data)  # CU设备数据包
         else:
-            print('Failed to send data:', response.text)
-    except Exception as e:
-        print('Error occurred while sending data:', e)
+            print(f"Unexpected device_type: {device_type} for fault_id: {fault_id}")
+
+    else:
+        print(f"Fault ID {fault_id} not found in mapping.")
 
 # MQTT 消息回调函数
 def on_message(client, userdata, msg):
     try:
-        # 转换消息为 UTF-8 编码的字符串，并打印
         payload_str = msg.payload.decode("utf-8")
         print("Received message:", payload_str)
 
-        # 將 JSON 字串解析為 Python 物件
         mqtt_data = json.loads(payload_str)
         fault_id = int(mqtt_data['notification']['alarm-notif']["fault-id"])
         is_cleared = mqtt_data['notification']['alarm-notif']['is-cleared']
 
-        # Update device status based on fault_id and is-cleared
         if fault_id in fault_to_device_mapping:
-            device_type, status_to_update = fault_to_device_mapping[fault_id]
-            
-            # If is_cleared is "false", set status to 3 (Idle state)
+            # 更新数据库状态
+            device_type, _ = fault_to_device_mapping[fault_id]
             if is_cleared == "false":
                 update_device_status(fault_id, device_type, 3, is_cleared)
-            # If is_cleared is "true", set status to 1 (Active state) and message to "The device is healthy"
             elif is_cleared == "true":
                 update_device_status(fault_id, device_type, 1, is_cleared)
 
-        # 生成要發送的 JSON 資料
-        json_payload = generate_json(mqtt_data)
-        # 调用发送 HTTP POST 请求的函数
-        send_http_request(json_payload)
+            # 处理告警并转发到上层装置
+            handle_fault(fault_id, is_cleared, mqtt_data)
+
     except Exception as e:
         print('Error occurred while processing message:', e)
 
